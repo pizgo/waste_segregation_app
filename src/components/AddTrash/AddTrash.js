f268d60aeda3402f5869ecda71ac630ee177bcb0
KPolrolniczak <k.polrolniczak@gmail.com>
2022-01-31 12:59:19 +0100

    working on addTrash

--- src/components/AddTrash/AddTrash.js
+++ src/components/AddTrash/AddTrash.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import {db} from "../resources/Firebase.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faNewspaper} from '@fortawesome/free-solid-svg-icons'
 
 import paper from '../../assets/paper.jpg';
 import bio from '../../assets/bio.jpg';
 import glass from '../../assets/glass.jpg';
 import mixed from '../../assets/mixed.jpg';
 import pet from '../../assets/pet.jpg';
+
 
 
 export const AddTrash = () => {
@@ -67,14 +70,14 @@ export const AddTrash = () => {
     return (
         <section className="addTrash">
                 <div className="addTrash__container">
-                    <p className="addTrash__hello">
+                    <p className="addTrash__text text__header">
                         Jeśli chcesz uzupełnić naszą bazę, wpisz w poniżej nazwę odpadu, wybierz, do którego pojemnika należy go wyrzucić i kliknij "Wyślij".
                         Zapoznamy się z Twoją sugestią.
                     </p>
                     <input type="text" className="addTrash__form" placeholder="Tu wpisz swoją sugestię" onChange={handleChange} />
                     <div className="addTrash__bins">
                         <img src={paper} alt="paper" className={`addTrash__bins-img ${clickedID === 1 && "addTrash__bins-img-clicked"} `} onClick={event => handleClick(1)}/>
-                        <img src={bio} alt="bio"     className={`addTrash__bins-img ${clickedID === 2 && "addTrash__bins-img-clicked"} `} onClick={event =>handleClick(2)}/>
+                        <img src={bio} alt="bio"     className={`addTrash__bins-img bio ${clickedID === 2 && "addTrash__bins-img-clicked"} `} onClick={event =>handleClick(2)}/>
                         <img src={glass} alt="glass" className={`addTrash__bins-img ${clickedID === 3 && "addTrash__bins-img-clicked"} `} onClick={event =>handleClick(3)}/>
                         <img src={mixed} alt="mixed" className={`addTrash__bins-img ${clickedID === 4 && "addTrash__bins-img-clicked"} `} onClick={event =>handleClick( 4)}/>
                         <img src={pet} alt="pet"     className={`addTrash__bins-img ${clickedID === 5 && "addTrash__bins-img-clicked"} `} onClick={event =>handleClick(5)}/>
@@ -83,6 +86,7 @@ export const AddTrash = () => {
                     <button className="addTrash__button" onClick={handleSubmit}>Wyślij!</button>
                         <p className="addTrash__error">{error}</p>
                         <p className="addTrash__success">{success}</p>
+                    <FontAwesomeIcon icon={faNewspaper}/>
                 </div>
         </section>
     )
