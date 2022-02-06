1c353b1f62be97740016d1b7028b019e2e5b5348
Katarzyna Półrolniczak <k.polrolniczak@gmail.com>
2022-02-06 15:49:37 +0100

    config for openstreetmap

--- src/components/CollectionMap/CollectionMap.js
+++ src/components/CollectionMap/CollectionMap.js
@@ -1,33 +1,8 @@
 import React from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps';
-
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 export const CollectionMap = () => {
-
-    function Map() {
-        return (
-            <GoogleMap
-                defaultZoom={10}
-                defaultCenter={{lat: 52.229675, lng: 21.012230}}/>)};
-
-    const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-    return (
-        <div className="container" style={{width: '60vw', height: '100vh'}}>
-            <section className='map'>
-                <div className='map__container'>
-                    <WrappedMap
-                    googleMapURL='https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places'
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `500px` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                    />
-                </div>
-            </section>
-        </div>
-    );
-
 
 }
 
